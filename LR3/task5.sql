c9f0f8da7399b6ac717c450116e30117c88c25f8
Adel Irshikeeva <adel.irshikeeva.04@gmail.com>
2023-11-02 22:35:09 +0400

    lr3

--- LR3/task5.sql
+++ LR3/task5.sql
@@ -1,6 +1,6 @@
 /*Выберите ФИО (== имя + фамилия) всех, кто покупал корты 1 и 2.*/
 USE cd;
-SELECT DISTINCT firstname, surname FROM members
-JOIN bookings ON bookings.memid = members.memid
-JOIN facilities ON bookings.facid = facilities.facid
-WHERE facility like '%Tennis Court%' AND firstname NOT like 'GUEST';
+SELECT DISTINCT surname, firstname FROM members
+JOIN bookings ON members.memid = bookings.memid
+JOIN facilities ON facilities.facid = bookings.facid
+WHERE facilities.facility LIKE '%Tennis court%';
---
+++ LR3/task7.sql
@@ -0,0 +1,20 @@
+/* Выберите список всех членов, включая человека, который их рекомендовал (если таковой имеется),
+без использования каких-либо объединений. Исключите в списке дубликаты,
+упорядочите лист по ФИО (==   имя + фамилия). */
+USE cd;
+SELECT 
+    concat(m1.surname, ' ', m1.firstname) AS fiomember
+FROM 
+    members m1
+WHERE 
+    EXISTS (
+        SELECT 1
+        FROM members AS m2
+        WHERE m2.memid = m1.recommendedby
+    )
+OR 
+    NOT EXISTS (
+        SELECT 1
+        FROM members AS m3
+        WHERE m3.memid = m1.recommendedby
+    )
