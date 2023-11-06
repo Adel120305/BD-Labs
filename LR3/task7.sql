32835c5a4aa227bce915c978f8326b6010f5e936
Adel Irshikeeva <adel.irshikeeva.04@gmail.com>
2023-11-06 16:15:46 +0400

    LR3

--- LR3/task7.sql
+++ LR3/task7.sql
@@ -1,20 +1,8 @@
 /* Выберите список всех членов, включая человека, который их рекомендовал (если таковой имеется),
 без использования каких-либо объединений. Исключите в списке дубликаты,
 упорядочите лист по ФИО (==   имя + фамилия). */
-USE cd;
-SELECT 
-    concat(m1.surname, ' ', m1.firstname) AS fiomember
-FROM 
-    members m1
-WHERE 
-    EXISTS (
-        SELECT 1
-        FROM members AS m2
-        WHERE m2.memid = m1.recommendedby
-    )
-OR 
-    NOT EXISTS (
-        SELECT 1
-        FROM members AS m3
-        WHERE m3.memid = m1.recommendedby
-    )
+use cd;
+SELECT DISTINCT concat(mem.surname,' ',mem.firstname) AS membername,
+concat(rec.surname,' ',rec.firstname) AS recname
+FROM members mem, (SELECT memid, recommendedby, surname, firstname FROM members) rec 
+WHERE mem.recommendedby=rec.memid;
