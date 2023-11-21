97d0f74d564329b52e18c45cb0829fb546d6aa50
Adel Irshikeeva <adel.irshikeeva.04@gmail.com>
2023-11-21 11:58:37 +0400

    LR5

--- LR5/task14.sql
+++ LR5/task14.sql
@@ -1,1 +1,4 @@
 /*Выведите идентификатор объекта, в котором забронировано наибольшее количество мест (slot). Убедитесь, что в случае ничьей все результаты привязки будут выведены.*/
+USE cd;
+SELECT DISTINCT facid, slots FROM (SELECT facid, slots, RANK() OVER (ORDER BY slots DESC) as maxrank FROM bookings) ranked_bookings
+WHERE maxrank = 1;
