85df6b737d3dad4ac3dc24d28dc56392e6e99ec0
Adel Irshikeeva <adel.irshikeeva.04@gmail.com>
2023-11-26 23:45:35 +0400

    LR5

--- LR5/task15.sql
+++ LR5/task15.sql
@@ -1,6 +1,8 @@
 /*Составьте список участников (включая гостей) вместе с количеством часов, которые они забронировали для объекта,  округленным до ближайших десяти часов. Ранжируйте их по этой округленной цифре, получая в результате имя, фамилию, округленные часы и звание. Сортировка по званию, фамилии и имени.*/
 USE cd;
-SELECT CONCAT(m.firstname, ' ', m.surname) AS 'ФИО', ROUND(SUM(COALESCE(b.slots / 2, 0)), -1) as 'Количество часов', 
-RANK() OVER (ORDER BY ROUND(SUM(COALESCE(b.slots / 2, 0)), -1)) as ranks
-FROM members m
-LEFT JOIN bookings b ON m.memid = b.memid GROUP BY m.memid ORDER BY m.surname, m.firstname;
+SELECT mem.firstname AS 'Имя', mem.surname AS 'Фамилия', ROUND(SUM(COALESCE(book.slots / 2, 0)) -1) AS 'Округлённые часы',
+RANK() OVER (ORDER BY ROUND(SUM(COALESCE(book.slots / 2, 0)), -1)) AS Ранг
+FROM members AS mem
+LEFT JOIN bookings AS book ON mem.memid = book.memid
+GROUP BY mem.memid
+ORDER BY Ранг, mem.surname, mem.firstname;
