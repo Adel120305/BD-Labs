3db0eebf9c7e84a8a7cac1a5e5c6af731337b38e
Adel Irshikeeva <adel.irshikeeva.04@gmail.com>
2023-11-27 10:19:22 +0400

    LR5

--- LR5/task11.sql
+++ LR5/task11.sql
@@ -1,5 +1,9 @@
 /*Составьте список общего количества часов, забронированных на один объект, помня, что интервал длится полчаса. Выходная таблица должна состоять из идентификатора объекта, имени и забронированных часов, отсортированных по идентификатору объекта. Попробуйте отформатировать часы с точностью до двух десятичных знаков.*/
 -- Используем базу данных "cd"
 USE cd;
-SELECT b.facid, f.facility, ROUND(SUM(b.slots) / 2, 2) AS 'Забронированные часы' FROM bookings b
-LEFT JOIN facilities f ON b.facid = f.facid GROUP BY b.facid ORDER BY b.facid;
+SELECT book.facid AS 'ID объекта', fac.facility AS 'Название объекта', 
+  ROUND(SUM(book.slots) / 2, 2) AS 'Общее количество часов'
+FROM bookings AS book
+INNER JOIN facilities AS fac ON book.facid = fac.facid
+GROUP BY book.facid, fac.facility
+ORDER BY book.facid;
